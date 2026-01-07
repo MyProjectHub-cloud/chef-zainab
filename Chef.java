e57cf8a664b0dd1d6a01b0db0eeec85bc7a6a9e5
MyProjectHub-cloud <255895@astanait.edu.kz>
2026-01-07 23:37:38 +0430

    Chef.java

--- Chef.java
+++ Chef.java
@@ -1,11 +1,45 @@
 public class Chef extends Staff {
 
-    public Chef(String name) {
-        super(name);
+    private String specialization;
+
+    public Chef(int staffId, String name, double salary, int experienceYears, String specialization) {
+        super(staffId, name, salary, experienceYears); // super MUST be first
+        setSpecialization(specialization);
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        if (specialization != null && !specialization.isEmpty()) {
+            this.specialization = specialization;
+        }
+    }
+
+    // Method overriding
+    @Override
+    public void work() {
+        System.out.println("Chef " + name + " is cooking " + specialization + " dishes.");
     }
 
     @Override
-    public void work() {
-        System.out.println("Chef " + name + " is cooking Kazakh Cuisine dishes.");
+    public String getRole() {
+        return "Chef";
+    }
+
+    // Child-specific methods
+    public void cookDish(String dish) {
+        System.out.println("Chef " + name + " prepares " + dish);
+    }
+
+    public boolean isMasterChef() {
+        return experienceYears >= 10;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Specialization: " + specialization;
     }
 }
+
